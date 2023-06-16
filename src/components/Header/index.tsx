0ef659c7c988a462b9ca28451bd5f3af510f961f
mathers08 <black.mathers08@mail.ru>
2023-06-16 16:20:11 +0300

    seo updates

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -21,11 +21,11 @@ const Header: FC<HeaderProps> = ({ onContactClick }) => {
             <p>Воронежский государственный университет</p>
             <p>Факультет прикладной математики, информатики и механики</p>
           </div>
-          <div onClick={onContactClick} className="info__list">
+          <div itemScope itemType="http://schema.org/Organization" onClick={onContactClick} className="info__list">
             <div className="info__list-item">
               <img src={phone} alt="phone" className="item-img" />
               <div className="item__info">
-                <div className="item__info-text">Связь:</div>
+                <div itemProp='connection' className="item__info-text">Связь:</div>
                 <div className="item__info-strong">+7 (473) 220-82-82</div>
                 <div className="item__info-text">mmio@amm.vsu.ru</div>
               </div>
@@ -33,7 +33,7 @@ const Header: FC<HeaderProps> = ({ onContactClick }) => {
             <div className="info__list-item">
               <img src={time} alt="time" className="item-img" />
               <div className="item__info">
-                <div className="item__info-text">Время работы:</div>
+                <div itemProp='work' className="item__info-text">Время работы:</div>
                 <div className="item__info-strong">8:30 - 16:30</div>
                 <div className="item__info-text">сб, вс - выходной</div>
               </div>
@@ -41,7 +41,7 @@ const Header: FC<HeaderProps> = ({ onContactClick }) => {
             <div className="info__list-item">
               <img src={address} alt="address" className="item-img" />
               <div className="item__info">
-                <div className="item__info-text">Адрес:</div>
+                <div itemProp='address' className="item__info-text">Адрес:</div>
                 <div className="item__info-strong">
                   г. Воронеж, Университетская пл. 1, каб. 317, кафедра ММИО
                 </div>
